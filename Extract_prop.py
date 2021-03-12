7cbfad20d3a0ba75c83983d272c2620a96ca4d51
Arnaldo-iq <39299850+Arnaldo-iq@users.noreply.github.com>
2021-03-12 18:54:38 +0000

    Update Extract_prop.py

--- Extract_prop.py
+++ Extract_prop.py
@@ -47,8 +47,20 @@ for i in files:
             valor = min(data)
       dockscore.append(valor)
 
+aro = [ ]
+PSA = [ ]
+MolW = [ ]
+NAc = [ ]
+NDon = [ ]
+ratio = [ ] 
+sp3Frac = [ ]
+LLE = [ ] 
+LE = [ ] 
+
 xlogp = [ ]
 PFI = [ ] 
+ScScore = [ ]
+SAR  = [ ]
 
 for i in files:
    with open(i, 'r') as inp3:
@@ -56,31 +68,44 @@ for i in files:
          if 'TIME:' in line:
             data = []
             data = line.split()
+            scs_dat = str(data[25])
+            sar_dat = str(data[13])
             pfi_dat = str(data[7])
-            logp = str(data[3])
+            logp_dat = str(data[3])
+            aro_dat = str(data[5])
+            PSA_dat = str(data[9]) 
+            MolW_dat = str(data[11])  
+            NAc_dat = str(data[16])
+            NDon_dat = str(data[19])
+            ratio_dat = str(data[28])
+            sp3_dat = str(data[32])
+            LLE_dat = str(data[35])
+            LE_dat = str(data[38])
+
+            ScScore.append(scs_dat)
+            SAR.append(sar_dat)
             PFI.append(pfi_dat)
-            xlogp.append(logp)
+            xlogp.append(logp_dat)
+            aro.append(aro_dat)
+            PSA.append(PSA_dat)
+            MolW.append(MolW_dat)
+            NAc.append(NAc_dat)
+            NDon.append(NDon_dat)
+            ratio.append(ratio_dat)
+            sp3Frac.append(sp3_dat)
+            LLE.append(LLE_dat)
+            LE.append(LE_dat)
 
-#print(xlogp)
-#print(PFI)
-print(len(xlogp))
-print(len(PFI))
-print(len(smiles))
-#
-#with open('gen', 'w') as inp:
-#   for (a, b) in zip(smiles, num): 
-#      inp.write(a + " " + b + "\n")
 
-#
-with open('gen2', 'w') as inp:
-   inp.write("id," + "smiles," + "chemgauss4," + "logp," + "PFI" )
+with open('gen2.csv', 'w') as inp:
+   inp.write("id," + "smiles," + "MPO_SCORE," + "pIC50," + "PFI," + "logp," + "Ar_rings," + "PSA," +  "MolW," + "NAc," + "NDon," + "(C)/(O)(N)_Ratio," + "sp3_frac," + "LLE," + "LE," + "ScScore" )
    inp.write('\n')
-   for (a, b, c, d, e) in zip(num, smiles, dockscore, xlogp, PFI):
-      inp.write(a + "," + str(b) + "," + str(c) + "," + str(d) + "," + str(e) + "\n")
+   for (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) in zip(num, smiles, dockscore, SAR, PFI, xlogp, aro, PSA, MolW, NAc, NDon, ratio, sp3Frac, LLE, LE, ScScore):
+      inp.write(str(a) + "," + str(b) + "," + str(c) + "," + str(d) + "," + str(e) + "," + str(f) + "," + str(g) + "," + str(h) + "," + str(i) + "," + str(j) + "," + str(k) + "," + str(l) + "," + str(m) + "," + str(n) + "," + str(o) + "," + str(p) + "\n")
 #
-
-#with open('gen1', 'w') as inp:
-#   inp.write("ID " + "chemgauss4")
-#   inp.write('\n')
-#   for (c, d) in zip(num, dockscore):
-#      inp.write(c + " " + str(d) + "\n")
+print( 'number of candidates is', len(smiles))
+print(len(dockscore))
+print(len(PFI))
+print(len(SAR))
+print(len(xlogp))
+print(len(ScScore))
